7f705843002207d35b9aeff0890600e102458e02
serverBKD <carlosm1bravoa@gmail.com>
2025-05-13 12:18:57 -0400

    2025MAY.13: v0.1.4d: vercel.json { "version": 2, "rewrites": [{ "source": "/(.*)", "destination": "/api" }] }

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,9 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_service_1 = __importDefault(require("./services/app.service"));
+const app_1 = __importDefault(require("./services/app"));
 const product_route_1 = __importDefault(require("./routes/product.route"));
-app_service_1.default.use("/api/products", product_route_1.default);
-app_service_1.default.get("/", (_req, res) => {
-    res.send("Hello World!");
+const client_route_1 = __importDefault(require("./routes/client.route"));
+const task_route_1 = __importDefault(require("./routes/task.route"));
+const hanyu_route_1 = __importDefault(require("./routes/hanyu.route"));
+const venta_route_1 = __importDefault(require("./routes/venta.route"));
+const blog_route_1 = __importDefault(require("./routes/blog.route"));
+const listemail_route_1 = __importDefault(require("./routes/listemail.route"));
+const log_route_1 = __importDefault(require("./routes/log.route"));
+const auth_route_1 = __importDefault(require("./routes/auth.route"));
+const provider_route_1 = __importDefault(require("./routes/provider.route"));
+app_1.default.use('/api/products', product_route_1.default);
+app_1.default.use('/api/clients', client_route_1.default);
+app_1.default.use('/api/tasks', task_route_1.default);
+app_1.default.use('/api/hanyu', hanyu_route_1.default);
+app_1.default.use('/api/ventas', venta_route_1.default);
+app_1.default.use('/api/blog', blog_route_1.default);
+app_1.default.use('/api/listemail', listemail_route_1.default);
+app_1.default.use('/api/logs', log_route_1.default);
+app_1.default.use('/api/auth', auth_route_1.default);
+app_1.default.use('/api/providers', provider_route_1.default);
+app_1.default.get('/', (_req, res) => {
+    res.send('xserver!');
 });
